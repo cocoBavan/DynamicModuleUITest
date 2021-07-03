0a62a53008ff5ec1f359714ad9df45c925de26fe
Bavananth Thavapalan <>
2021-07-04 00:52:41 +1000

    Superb Text

--- dynoninstall/src/main/java/com/bltech/dynoninstall/DynamicOnInstall.kt
+++ dynoninstall/src/main/java/com/bltech/dynoninstall/DynamicOnInstall.kt
@@ -1,11 +1,20 @@
 package com.bltech.dynoninstall
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DynamicOnInstall : AppCompatActivity() {
+
+    var currentNumber = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oninstall)
+
+        findViewById<Button>(R.id.buttononinstall).setOnClickListener {
+            currentNumber *= 2
+            findViewById<TextView>(R.id.textView).text = "$currentNumber"
+        }
     }
 }
